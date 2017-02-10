47361bfa35e90fc68c0388c067f9939a1efa5b21
Serge Lerner <serge@yodas.com>
2017-02-11 00:01:00 +0200

    Ajust build and deploy

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,7 @@ module.exports = {
   output: {
     path: staticsPath,
     filename: (isDeploy) ? 'main.[hash].js' : 'main.js',
-    publicPath: '/',
+    publicPath: '/puteshestvennik',
   },
   module: {
     rules: [
