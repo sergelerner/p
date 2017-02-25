2d3037d70eee12d715e161ef9099daad0d60da24
Serge Lerner <serge@yodas.com>
2017-02-25 13:14:30 +0200

    Format tour link

--- client/reducers/vouchers.reducer.js
+++ client/reducers/vouchers.reducer.js
@@ -72,6 +72,7 @@ const createBody = (rows) => {
     departure: rangeDate(row.departure, row.arrival),
     price: `${row.price}${row.coin}|${row.price - row.discount}${row.coin}`,
     company: `${row.company} | ${row.guide}`,
+    tourl: row.tourl.split('=')[1],
   }));
 
   return body;
