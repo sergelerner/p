3356bbe0f006a8114c27cf791c591a5437726812
Serge Lerner <serge@yodas.com>
2017-06-03 12:42:31 +0300

    Remove filter and vouchers for now

--- client/containers/main.container.jsx
+++ client/containers/main.container.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import Filters from './filters.container.jsx';
-import Vouchers from './vouchers.container.jsx';
 
 import SiteHeader from '../components/site-header.jsx';
 import HomeJumbotron from '../components/home-jumbotron.jsx';
@@ -10,11 +8,7 @@ class Main extends Component {
     return (
       <main className="main">
         <SiteHeader />
-
         <HomeJumbotron />
-
-        <Filters />
-        <Vouchers />
       </main>
     );
   }
